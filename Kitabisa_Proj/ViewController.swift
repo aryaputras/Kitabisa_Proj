887688919f86c962d106cbdd9223f75d06b4c678
aryaputras <abilabil147@gmail.com>
2021-10-16 16:05:36 +0700

    Initial Commit

---
+++ Kitabisa_Proj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kitabisa_Proj
+//
+//  Created by Abigail Aryaputra Sudarman on 16/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
